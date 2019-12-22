2e515f659dba2b41481fd3474de37fd5cdc5dfa0
Ethan Atkins <contact@ethanatkins.com>
2019-12-21 18:00:02 -0800

    ok

--- client/src/main/java/sbt/client/Client.java
+++ client/src/main/java/sbt/client/Client.java
@@ -1,15 +1,11 @@
 package sbt.client;
 
-import java.io.File;
 import sbt.internal.client.SimpleClient;
-import scala.collection.immutable.List$;
 
 public class Client {
   public static void main(final String[] args) {
     try {
-      final File file =
-          args.length == 0 ? new File("").getCanonicalFile() : new File(args[0]).getCanonicalFile();
-      new SimpleClient(file, List$.MODULE$.empty());
+      SimpleClient.apply(args);
     } catch (final Throwable t) {
       t.printStackTrace();
     }
