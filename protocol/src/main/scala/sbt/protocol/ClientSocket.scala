2e67df7cef677634854c77a091969b59ef97785c
Ethan Atkins <contact@ethanatkins.com>
2020-05-02 11:13:22 -0700

    provider revert

--- protocol/src/main/scala/sbt/protocol/ClientSocket.scala
+++ protocol/src/main/scala/sbt/protocol/ClientSocket.scala
@@ -21,10 +21,7 @@ import org.scalasbt.ipcsocket._
 object ClientSocket {
   private lazy val fileFormats = new BasicJsonProtocol with PortFileFormats with TokenFileFormats {}
 
-  def socket(
-      portfile: File,
-      provider: UnixDomainSocketLibraryProvider
-  ): (Socket, Option[String]) = {
+  def socket(portfile: File): (Socket, Option[String]) = {
     import fileFormats._
     val json: JValue = Parser.parseFromFile(portfile).get
     val p = Converter.fromJson[PortFile](json).get
@@ -45,6 +42,4 @@ object ClientSocket {
     }
     (sk, token)
   }
-  def socket(portfile: File): (Socket, Option[String]) =
-    socket(portfile, UnixDomainSocketLibraryProvider.jna)
 }
