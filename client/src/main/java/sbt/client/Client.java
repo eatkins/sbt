d2b0750b0d0f32f3323120a4c546bdeda732f65a
Ethan Atkins <contact@ethanatkins.com>
2020-05-21 21:00:06 -0700

    jansi

--- client/src/main/java/sbt/client/Client.java
+++ client/src/main/java/sbt/client/Client.java
@@ -5,9 +5,11 @@ import sbt.internal.client.SimpleClient;
 public class Client {
   public static void main(final String[] args) {
     try {
+      JAnsi.install();
       SimpleClient.apply(args);
     } catch (final Throwable t) {
       t.printStackTrace();
     }
+    JAnsi.uninstall();
   }
 }
