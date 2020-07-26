f3529e78a5bd757627023fff479c4b0fa466faf1
Ethan Atkins <contact@ethanatkins.com>
2020-07-26 12:28:50 -0700

    temp jline 3 version;

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -85,7 +85,8 @@ object Dependencies {
 
   val jline = "org.scala-sbt.jline" % "jline" % "2.14.7-sbt-5e51b9d4f9631ebfa29753ce4accc57808e7fd6b"
   val jline3Version = "3.16.0"
-  val jline3Terminal = "org.scala-sbt.jline3" % "jline-terminal" % s"$jline3Version-sbt-d5b4c3b56243612fdccd1751ffa1db6d5921eb1f"
+  //val jline3Terminal = "org.scala-sbt.jline3" % "jline-terminal" % s"$jline3Version-sbt-d5b4c3b56243612fdccd1751ffa1db6d5921eb1f"
+  val jline3Terminal = "org.jline" % "jline-terminal" % jline3Version
   val jline3Jansi = "org.jline" % "jline-terminal-jansi" % jline3Version
   val jline3Reader = "org.jline" % "jline-reader" % jline3Version
   val jansi = "org.fusesource.jansi" % "jansi" % "1.18"
