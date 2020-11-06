05dbec0c1fd4e44ffe6423a35740d8541be9d222
Ethan Atkins <contact@ethanatkins.com>
2020-11-06 15:03:17 -0800

    debug

--- server-test/src/test/scala/testpkg/ClientTest.scala
+++ server-test/src/test/scala/testpkg/ClientTest.scala
@@ -85,80 +85,82 @@ object ClientTest extends AbstractServerTest {
       )
     )
   }
-  private def client(args: String*): Int = client(NullInputStream, args: _*)
-  // This ensures that the completion command will send a tab that triggers
-  // sbt to call definedTestNames or discoveredMainClasses if there hasn't
-  // been a necessary compilation
-  def tabs = new FixedInputStream('\t', '\t')
-  private def complete(completionString: String): Seq[String] = {
-    val cps = new CachingPrintStream
-    background(
-      NetworkClient.complete(
-        testPath.toFile,
-        Array(s"--completions=sbtn $completionString"),
-        false,
-        tabs,
-        cps
-      )
-    )
-    cps.lines
-  }
-  test("exit success") { c =>
-    assert(client("willSucceed") == 0)
-  }
-  test("exit failure") { _ =>
-    assert(client("willFail") == 1)
-  }
-  test("two commands") { _ =>
-    assert(client("compile;willSucceed") == 0)
-  }
-  test("two commands with failing second") { _ =>
-    assert(client("compile;willFail") == 1)
-  }
-  test("two commands with leading failure") { _ =>
-    assert(client("willFail;willSucceed") == 1)
-  }
-  test("three commands") { _ =>
-    assert(client("compile;clean;willSucceed") == 0)
-  }
-  test("three commands with middle failure") { _ =>
-    assert(client("compile;willFail;willSucceed") == 1)
-  }
-  test("compi completions") { _ =>
-    val expected = Vector(
-      "compile",
-      "compile:",
-      "compileAnalysisFile",
-      "compileAnalysisFilename",
-      "compileAnalysisTargetRoot",
-      "compileEarly",
-      "compileIncSetup",
-      "compileIncremental",
-      "compileJava",
-      "compileOutputs",
-      "compileProgress",
-      "compileScalaBackend",
-      "compileSplit",
-      "compilers",
-    )
-
-    assert(complete("compi") == expected)
-  }
-  test("testOnly completions") { _ =>
-    val testOnlyExpected = Vector(
-      "testOnly",
-      "testOnly/",
-      "testOnly::",
-      "testOnly;",
-    )
-    assert(complete("testOnly") == testOnlyExpected)
-
-    val testOnlyOptionsExpected = Vector("--", ";", "test.pkg.FooSpec")
-    assert(complete("testOnly ") == testOnlyOptionsExpected)
-  }
-  test("quote with semi") { _ =>
-    assert(complete("\"compile; fooB") == Vector("compile; fooBar"))
-  }
+  /*
+   *  private def client(args: String*): Int = client(NullInputStream, args: _*)
+   *  // This ensures that the completion command will send a tab that triggers
+   *  // sbt to call definedTestNames or discoveredMainClasses if there hasn't
+   *  // been a necessary compilation
+   *  def tabs = new FixedInputStream('\t', '\t')
+   *  private def complete(completionString: String): Seq[String] = {
+   *    val cps = new CachingPrintStream
+   *    background(
+   *      NetworkClient.complete(
+   *        testPath.toFile,
+   *        Array(s"--completions=sbtn $completionString"),
+   *        false,
+   *        tabs,
+   *        cps
+   *      )
+   *    )
+   *    cps.lines
+   *  }
+   *  test("exit success") { c =>
+   *    assert(client("willSucceed") == 0)
+   *  }
+   *  test("exit failure") { _ =>
+   *    assert(client("willFail") == 1)
+   *  }
+   *  test("two commands") { _ =>
+   *    assert(client("compile;willSucceed") == 0)
+   *  }
+   *  test("two commands with failing second") { _ =>
+   *    assert(client("compile;willFail") == 1)
+   *  }
+   *  test("two commands with leading failure") { _ =>
+   *    assert(client("willFail;willSucceed") == 1)
+   *  }
+   *  test("three commands") { _ =>
+   *    assert(client("compile;clean;willSucceed") == 0)
+   *  }
+   *  test("three commands with middle failure") { _ =>
+   *    assert(client("compile;willFail;willSucceed") == 1)
+   *  }
+   *  test("compi completions") { _ =>
+   *    val expected = Vector(
+   *      "compile",
+   *      "compile:",
+   *      "compileAnalysisFile",
+   *      "compileAnalysisFilename",
+   *      "compileAnalysisTargetRoot",
+   *      "compileEarly",
+   *      "compileIncSetup",
+   *      "compileIncremental",
+   *      "compileJava",
+   *      "compileOutputs",
+   *      "compileProgress",
+   *      "compileScalaBackend",
+   *      "compileSplit",
+   *      "compilers",
+   *    )
+   *
+   *    assert(complete("compi") == expected)
+   *  }
+   *  test("testOnly completions") { _ =>
+   *    val testOnlyExpected = Vector(
+   *      "testOnly",
+   *      "testOnly/",
+   *      "testOnly::",
+   *      "testOnly;",
+   *    )
+   *    assert(complete("testOnly") == testOnlyExpected)
+   *
+   *    val testOnlyOptionsExpected = Vector("--", ";", "test.pkg.FooSpec")
+   *    assert(complete("testOnly ") == testOnlyOptionsExpected)
+   *  }
+   *  test("quote with semi") { _ =>
+   *    assert(complete("\"compile; fooB") == Vector("compile; fooBar"))
+   *  }
+   */
   test("multiple clients can simultaneously call run") { _ =>
     // In this test, we start two clients on background thread which both
     // call the project main class. The main class takes a filename as
@@ -171,8 +173,9 @@ object ClientTest extends AbstractServerTest {
     def blockUntilStart(file: File): Unit = {
       val limit = 30.seconds.fromNow
       while (Try(IO.read(file)) != Success("start") && !limit.isOverdue) {
-        Thread.sleep(20)
+        Thread.sleep(200)
       }
+      System.err.println(Try(IO.read(file)))
       if (limit.isOverdue) throw new TimeoutException
     }
     IO.withTemporaryDirectory { dir =>
